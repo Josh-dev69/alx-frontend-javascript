e9a3c31a30d3df6df91d57d0d68f6d778116a3c3
Josh-dev69 <ebubejoshua69@gmail.com>
2024-04-30 13:22:12 +0100

    Completed

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,9 +3,7 @@ export const weakMap = new WeakMap();
 // https://stackoverflow.com/questions/29413222/what-are-the-actual-uses-of-es6-weakmap
 export function queryAPI(endpoint) {
   let called = 0;
-  if (weakMap.get(endpoint))
-    called = weakMap.get(endpoint);
+  if (weakMap.get(endpoint)) called = weakMap.get(endpoint);
   weakMap.set(endpoint, called + 1);
-  if (called + 1 >= 5)
-    throw new Error('Endpoint load is high');
+  if (called + 1 >= 5) throw new Error('Endpoint load is high');
 }
