b58cd47903cd4326bf90ff7aabd0dca9e64bb0d6
Josh-dev69 <ebubejoshua69@gmail.com>
2024-04-30 12:52:14 +0100

    ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,6 @@ export default function getListStudentIds(studentList) {
   if (!Array.isArray(studentList)) {
     return [];
   }
-  return studentList.map(student => student.id);
+
+  return studentList.map((student) => student.id);
 }
